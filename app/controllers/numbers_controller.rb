cb8220b5f74a26a3e9a57133963bb4701246e352
chhavo <cadtani@gmail.com>
2015-04-27 17:18:07 -0500

    practice in class

---
+++ app/controllers/numbers_controller.rb
@@ -0,0 +1,14 @@
+class NumbersController < ApplicationController
+  def lucky
+
+      @number  = rand(1000)
+      render("lucky.html.erb")
+
+  end
+
+  def unlucky
+    @number = rand(1000)
+    render("unlucky.html.erb")
+  end
+
+end
